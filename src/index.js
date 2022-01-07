9046b1bf5fc0d2c072a90dc7a5b05e67fb6bb675
User <user@UserdeMacBook-Air.local>
2022-01-07 17:12:25 +0800

    add react files

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function App() {
+  return(
+    <div>
+
+      <h1>Profile</h1>
+      <p>Name: Joe C.</p>
+      <button>Get to know me</button>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
