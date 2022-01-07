b60c371fb8138258fc8dcd772d7a6ea4c002345c
Joe C <qq83323@gmail.com>
2022-01-08 02:44:28 +0800

    update Nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-function App() {
+import React from "react";
+
+const App = () => {
   return (
-    <div className="App">
+    <div>
+      <h1>My Profile</h1>
+      <p>My name is Joe.</p>
+      <p>I am 27 years old.</p>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Nav from "./Nav";
 
-
-function App() {
-  return(
-    <div>
-
-      <h1>Profile</h1>
-      <p>Name: Joe C.</p>
-      <button>Get to know me</button>
-    </div>
-  )
-}
-
-ReactDOM.render(App(),  document.getElementById('root')
+//In React.StrictMode, use self-closig tag to refer the app function.
+ReactDOM.render(
+  <React.StrictMode>
+    <Nav />
+    <App />
+  </React.StrictMode>,
+  document.querySelector("#root")
 );
-
