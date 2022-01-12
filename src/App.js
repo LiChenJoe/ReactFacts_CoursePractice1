a591e4c80897e3977709b110b9595d548e8b9111
Joe C <qq83323@gmail.com>
2022-01-13 01:15:48 +0800

    add style

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from "react";
 
 // One function set , section set are a component in React, and they are reuseable.
 const App = () => {
+  let friends = ["Adi", "Jessica", "Sama"];
   return (
     <div>
       <h1>My Profile</h1>
       <p>My name is Joe.</p>
       <p>I am 27 years old.</p>
+      <p>My frineds are: </p>
+      {friends.map((friend) => (
+        <p>{friend}</p>
+      ))}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Nav from "./Nav";
+import "./styles/style.css";
 
 //In React.StrictMode, use self-closig tag to refer the app function.
 ReactDOM.render(
